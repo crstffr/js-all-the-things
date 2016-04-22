2a9923dfe203f62e46a852cd8dd6d79e78e05787
Christopher Mason <crstffr@users.noreply.github.com>
2016-04-21 19:57:57 -0500

    Update app.module.js

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -2,5 +2,5 @@ var angular = require('angular');
 var controller = require('./app.ctrl');
 
 module.exports = angular
-    .module('biawak.module', [])
+    .module('app.module', [])
     .controller('AppController', controller);
